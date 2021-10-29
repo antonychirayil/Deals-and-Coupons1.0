15f471f9c05122b94a221cabf5391e8fd393b92e
antonychirayil <antonychirayil@users.noreply.github.com>
2021-10-29 17:13:40 +0530

    Added Coupon Module

--- Backend/CouponFinder/src/app/coupon/coupon.component.ts
+++ Backend/CouponFinder/src/app/coupon/coupon.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AdminService } from '../admin.service';
+import { coupon } from '../coupon';
 @Component({
   selector: 'app-coupon',
   templateUrl: './coupon.component.html',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CouponComponent implements OnInit {
 
-  constructor() { }
+  coupons: coupon[]= [];
+  formValue!:FormGroup;
+  userObj : coupon = new coupon("","","","","","");
+  result: any;
+  pcode: any;
+  edate:any
+
+  constructor(private adminService:AdminService, private fb:FormBuilder) { }
+
 
   ngOnInit(): void {
+
+    this.getCoupons()
+
+
+    this.formValue=this.fb.group({
+    id: [""],
+    provider: [""],
+    code: [""],
+    category: [""],
+    description: [""],
+    expiryDate: [""],
+    })
+
+
+
+
+  }
+  updatepcode(data: any,date:any){
+    this.pcode=data;
+    this.edate=date
+
+
   }
 
+  getCoupons(){
+    console.log('Test Call');
+     this.adminService.getAllCoupons().subscribe(res=>{this.coupons=res});
 }
+}
+
