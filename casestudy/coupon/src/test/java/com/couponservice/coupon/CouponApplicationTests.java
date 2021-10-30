40f1cb91c09cbe400747cf0ea487091e68e099a9
antonychirayil <antonychirayil@users.noreply.github.com>
2021-10-31 01:11:51 +0530

    Test Case For Coupon Added

--- casestudy/coupon/src/test/java/com/couponservice/coupon/CouponApplicationTests.java
+++ casestudy/coupon/src/test/java/com/couponservice/coupon/CouponApplicationTests.java
@@ -1,13 +1,54 @@
 package com.couponservice.coupon;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 class CouponApplicationTests {
 
+    @MockBean
+    CouponRepository couponRepository;
+
+    @Autowired
+    CouponController couponController;
+
+
     @Test
-    void contextLoads() {
+    public void getAllCouponsTest() {
+        when(couponRepository.findAll()).thenReturn(
+                Stream.of(
+                                new Coupon("", "amazon", "amaz12", "mobiles", "10% offer","12-10-2021"))
+                        .collect(Collectors.toList()));
+        assertEquals(1, couponController.getAllCoupons().size());
+
     }
 
+    @Test
+    public void addCouponTest() {
+        Coupon coupon = new Coupon("", "amazon", "ama12", "mobiles", "10% offer","20-10-2021");
+        when(couponRepository.save(coupon)).thenReturn(coupon);
+        assertEquals(coupon, couponController.addCoupon(coupon));
+    }
+
+    @Test
+    public void deleteDealTest() {
+
+        String couponId = "1";
+
+        Coupon coupon = new Coupon("1", "amazon", "ama12", "mobiles", "10% offer","12-10-21");
+        couponRepository.deleteById(couponId);
+        verify(couponRepository).deleteById(couponId);
+
+
+    }
 }
